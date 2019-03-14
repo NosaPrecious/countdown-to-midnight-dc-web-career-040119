697d4a79dce9fb9eaa01040de1345ffaa9b4355f
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-14 02:12:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,8 @@
 
 def countdown(number)
   counter = 0
-  number+= 2
   while counter < number
   counter += 1
-  break if counter == 12
   puts "#{counter} SECOND(S)!"
 end
-  puts "HAPPY NEW YEAR!"
 end
